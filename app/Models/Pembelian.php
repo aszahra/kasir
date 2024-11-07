1ff6908dbb8bf1de072777f59ab4d4686b476a24
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2024-11-07 16:24:20 +0700

    add pembelian model

--- app/Models/Pembelian.php
+++ app/Models/Pembelian.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Pembelian extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'kode_pembelian',        
+        'tgl_pembelian',        
+        'id_supplier',                
+        'id_user'        
+    ];
+
+    protected $table = 'penjualan';
 }
