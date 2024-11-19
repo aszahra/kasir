f6f564c26366f8964f10ee422ca15bd808265487
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2024-11-19 14:27:32 +0700

    add js for create.penjualan

--- resources/views/page/penjualan/create.blade.php
+++ resources/views/page/penjualan/create.blade.php
@@ -46,7 +46,7 @@
                             <div><button id="addRowBtn" class="bg-sky-400 hover:bg-sky-500 text-white px-2 rounded-xl"> Add Row</button></div>
                         </div>
                         <div class="border border-2 rounded-xl p-2 mb-2" id="produkContainer">
-                            <div class="border-2 flex gap-5 p-2 mb-2 rounded-xl">
+                            {{-- <div class="border-2 flex gap-5 p-2 mb-2 rounded-xl">
                                 <div class="mb-5 w-full">
                                     <label for="produk"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Produk</label>
@@ -71,7 +71,17 @@
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required />
                                 </div>
-                            </div>
+                                <div class="mb-5 w-full">
+                                    <label for="total_harga"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total Harga</label>
+                                    <input type="number" id="total_harga" name="total_harga"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required readonly />
+                                </div>
+                                <div class="px-2 bg-red-100">
+                                    Hapus
+                                </div>
+                            </div> --}}
                         </div>
                         <button type="submit"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
@@ -80,4 +90,20 @@
             </div>
         </div>
     </div>
+    <script>
+        $(document).ready(function () {
+            $('#addRowBtn').click(function(event) {
+                event.preventDefault();
+                addRow();
+            });
+        });
+
+        let rowCount = 0;
+
+        function addRow() {
+            rowCount++;
+            const newRow = '<div>coba</div>';
+            $('#produkContainer').append(newRow);
+        }
+    </script>
 </x-app-layout>
